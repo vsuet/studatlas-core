97c2a9b45ae22450c0c95b754a458a56a7067047
Vitaly Pereslavtsev <vitalyipereslavcev@noorsoft.ru>
2019-09-23 23:51:26 +0300

    добавил доп поля ведомости

--- src/documents/models/document.model.ts
+++ src/documents/models/document.model.ts
@@ -6,14 +6,44 @@ export class Document {
   @Field(type => ID, { nullable: true })
   id: string;
 
-  @Field()
+  @Field({ description: 'Блок' })
   unit: string;
 
-  @Field()
+  @Field({ description: 'Год контроля' })
   year: string;
 
-  @Field(type => Int)
+  @Field(type => Int, { description: 'Курс' })
   yearNumber: string;
+
+  @Field({ description: 'Преподаватель' })
+  teacher: string;
+
+  @Field(type => Int, { description: 'Семестр' })
+  semester: number;
+
+  @Field(type => Int, { description: 'Часов' })
+  hours: number;
+
+  @Field({ description: 'Дисциплина' })
+  subject: string;
+
+  @Field({ description: 'Тип ведомости' })
+  type: string;
+
+  @Field({ description: 'Кол-во сохранений' })
+  savesCount: number;
+
+  @Field({ description: 'Учебный план' })
+  curricula: string;
+
+  @Field({ description: 'Статус' })
+  status: string;
+
+  @Field({ description: 'Дата изменения' })
+  updatedAt: string;
+
+  @Field({ description: 'Компьютер' })
+  device: string;
 
   @Field(type => [DocumentMember], { nullable: true })
   members?: DocumentMember[];

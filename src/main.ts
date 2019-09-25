5463f3ff2ef46dcd09c44b255cd15b6cf67be6dc
Vitaly Pereslavtsev <vitalyipereslavcev@noorsoft.ru>
2019-09-26 00:11:44 +0300

    fastify, env fixes

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
-import * as csurf from 'csurf';
-import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
-  app.use(helmet());
-  app.use(csurf());
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
   );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

9e68275a1ceb1b9e32b7f36ffa40117c72cc7e81
Vitaly Pereslavtsev <vitalyipereslavcev@noorsoft.ru>
2019-10-02 23:49:26 +0300

    auth fixes

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,12 +2,10 @@ import { passportJwtSecret } from 'jwks-rsa';
 import { ExtractJwt, Strategy, VerifiedCallback } from 'passport-jwt';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import * as dotenv from 'dotenv';
-import * as fs from 'fs';
 import { JwtPayload } from './interfaces/jwt-payload.interface';
 
-// TODO: prod!!!!!
-const envConfig = dotenv.parse(fs.readFileSync('.env'));
+// tslint:disable-next-line:no-var-requires
+require('dotenv').config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -17,12 +15,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `https://${envConfig.AUTH0_DOMAIN}/.well-known/jwks.json`,
+        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
       }),
 
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // 1
-      audience: 'http://localhost:3000',
-      issuer: `https://${envConfig.AUTH0_DOMAIN}/`,
+      audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
+      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
     });
   }
 
